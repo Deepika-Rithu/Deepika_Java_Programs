988c1f97fed2140a7ee153fac96a90a6879aa659
Deepika <deepik27@gmail.com>
2024-07-16 19:42:36 +0530

    commit

---
+++ If_Else_If_Block.java
@@ -0,0 +1,27 @@
+package Package1;
+
+public class If_Else_If_Block 
+{
+public static void main(String[] args)
+	{
+	int a=10;
+	int b=20;
+	if(a>b)
+	{
+		System.out.println("1");
+	}
+	else if(a==b)
+	{
+		System.out.println("2");
+	}
+	else if(a<=b)
+	{
+		System.out.println("3");
+	}
+	else
+	{
+		System.out.println("4");
+	}
+
+}
+}
